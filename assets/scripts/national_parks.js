c1876d90fbe402082b96a2e1d666423fc7500652
Lyn <chap202@gmail.com>
2022-06-29 14:46:00 -0500

    Eric helped me get the drop downs working.

--- assets/scripts/national_parks.js
+++ assets/scripts/national_parks.js
@@ -3,35 +3,104 @@
 
 // Everything is going to be event driven based on user input.//
 
+// Get the dropdown list in the JS
+let searchTypeDDL = document.querySelector("#searchTypeDDL");
+let locationsDDL = document.querySelector("#locations");
+let typesDDL = document.querySelector("#types");
+let stateSearchList = document.querySelector("#stateSearchList");
 
-let theSampleButton = document.querySelector("#theDemoButton")
-let theSampleUL = document.querySelector("#locations")
-let parkTypeUL = document.querySelector("#partTypes")
-let parksUL = document.querySelector("#parks")
+
+// theSampleButton.addEventListener("click", function(event){
+//     console.log(locationsArray)
+//     console.log(nationalParksArray)
+//     console.log(parkTypesArray)
 
 
-theSampleButton.addEventListener("click", function(event){
-    console.log(locationsArray)
-    console.log(nationalParksArray)
-    console.log(parkTypesArray)
+//     locationsArray.forEach((location) => {
+//         theSampleUL.innerHTML += `<li>${location}</li>`
+//     })
 
+//     parkTypesArray.forEach((parkType) => {
+//         parkTypeUL.innerHTML += `<li>${parkType}</li>`
+//     })
+
+//     nationalParksArray.forEach((park) => {
+//         parksUl.innerHTML += `<li>${park.LocationName}</li>`
+//     })
+
+//     theSampleUL.classList.remove("d-none")
+
+//     parkTypeUL.classList.remove("d-none")
+
+//     parksUL.classList.remove("d-none")
+
+// })
+
+
+
+
+searchTypeDDL.addEventListener("change", function (event) {
+    alert("dropdown changed to:" + event.target.value)
+
+    // check which search type
+    // if search type is type then show types dropdown
+    // if search type is location then show locations dropdown
+
+    //hide all the dropdowns and then check which is supposed to show below
+    //     locationsDDL.classList.add("d-none")
+    //     typesDDL.classList.add("d-none")
+    stateSearchList.classList.add("d-none")
+
+
+
+    if (event.target.value === "location") {
+        generateLocationsDDLOptions();
+        stateSearchList.classList.remove("d-none")
+
+
+    }
+
+    // if(event.target.value === "type"){
+    //     generateTypesDDLOptions()
+    //     typesDDL.classList.remove("d-none")
+    //     locationsDDL.classList.add("d-none")
+
+    // }
+})
+stateSearchList.addEventListener("change", function (event) {
+    alert("dropdown changed to:" + event.target.value)
+    let filteredParksArray = nationalParksArray.filter(function (park) {
+        if (park.State === event.target.value) {
+            return true
+        }
+        return false
+
+    })
+    console.log(filteredParksArray)
+})
+
+
+
+
+// locationsDDL.addEventListener("change", function(event){
+//     alert(event.target.value)
+// })
+
+function generateLocationsDDLOptions() {
+
+    stateSearchList.innerHTML = `<option value="">Choose A Location</option>`
 
     locationsArray.forEach((location) => {
-        theSampleUL.innerHTML += `<li>${location}</li>`
+        stateSearchList.innerHTML += `<option value="${location}">${location}</option>`
     })
 
-    parkTypesArray.forEach((parkType) => {
-        parkTypeUL.innerHTML += `<li>${parkType}</li>`
+}
+
+function generateTypesDDLOptions() {
+
+    parkTypesArray.forEach((partkType) => {
+        typesDDL.innerHTML += `<option value="${partkType}">${partkType}</option>`
     })
 
-    nationalParksArray.forEach((park) => {
-        parksUl.innerHTML += `<li>${park.LocationName}</li>`
-    })
+}
 
-    theSampleUL.classList.remove("d-none")
-
-    parkTypeUL.classList.remove("d-none")
-
-    parksUL.classList.remove("d-none")
-
-})
